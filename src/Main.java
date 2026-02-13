f9746fd205da5e81ee87835370cebffcffc02a98
nicolle <nicolle.noriegacelis@gmail.com>
2026-02-13 16:54:08 -0500

    nombre y lenguaje

--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,5 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println ( "primeros comandos de git");
-
+        //Nicolle Noriega celis//
+        //lenguaje (python)//
